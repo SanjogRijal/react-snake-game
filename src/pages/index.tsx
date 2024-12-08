524cafc688d19b9531a54984c48a3c4b3f90f2f1
SanjogRijal <sanjog@fusemachines.com>
2024-12-09 02:02:33 +0545

    feat: new design and arrows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,10 @@ export default function Home() {
   const isMobile = useDeviceType();
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} ${
-        !isMobile &&
-        "grid grid-rows-[1fr_auto_50px] items-center justify-items-center min-h-[100vh] p-8 pb-16 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-white bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-cover bg-center"
-      } grid grid-rows-[1fr_auto_50px] items-center justify-items-center min-h-[100vh] p-8 pb-16 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable}
+          grid grid-rows-[1fr_auto_57vh] items-center justify-items-center min-h-[100vh] p-8 pb-16 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] text-white bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-cover bg-center ${
+            isMobile && "w-[685px]"
+          }`}
     >
       <main className="flex flex-col items-center justify-center gap-8 row-start-1 text-center sm:text-left">
         <h1 className="text-3xl sm:text-5xl font-semibold leading-tight tracking-wide">
